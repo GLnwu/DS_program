c8d8db4a32d52d00d5c9d24be8e51d148499f066
GLnwu <bingtang1021@gmail.com>
2015-07-19 14:48:23 +0800

    reverse the self define type Status,let it be a bool value rather a int value

--- DS.h
+++ DS.h
@@ -4,12 +4,13 @@
     > Mail: bingtang1021@gmail.com 
     > Created Time: 2015年07月19日 星期日 12时57分45秒
  ************************************************************************/
+#include<stdbool.h>
 #define TRUE 		1
 #define FALSE 		0
 #define OK 		1
 #define ERROR		0
 #define INFEASIBLE 	-1
 #define OVERFLOW	-2
-//Status 是函数的类型，其值是函数结果状态代码
-typedef int Status;
+//Status 是函数的类型，定义为bool类型，值为true或false
+typedef bool Status;
 
