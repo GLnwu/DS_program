9c5e1cb4d7a98b8e405dbd269af1818ff3078298
GLnwu <bingtang1021@gmail.com>
2015-07-19 13:01:19 +0800

    add DS.h

---
+++ DS.h
@@ -0,0 +1,15 @@
+/*************************************************************************
+    > File Name: DS.h
+    > Author: GaoLi
+    > Mail: bingtang1021@gmail.com 
+    > Created Time: 2015年07月19日 星期日 12时57分45秒
+ ************************************************************************/
+#define TRUE 		1
+#define FALSE 		0
+#define OK 		1
+#define ERROR		0
+#define INFEASIBLE 	-1
+#define OVERFLOW	-2
+//Status 是函数的类型，其值是函数结果状态代码
+typedef int Status;
+
